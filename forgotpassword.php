b179471f2d5d2598afbdaad06e38fa450adb5e06
Sumit Kumar <sumitthakur1600@gmail.com>
2024-10-02 14:06:57 +0530

    Update forgotpassword.php

--- forgotpassword.php
+++ forgotpassword.php
@@ -27,11 +27,11 @@ if (isset($_POST['submit_email'])) {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'dewan201968@gmail.com'; // Your email
-            $mail->Password = 'yaodyxvdrrfynpgl'; // Your email password
+            $mail->Username = ''; // Your email
+            $mail->Password = ''; // Your email password
             $mail->SMTPSecure = 'ssl';
             $mail->Port = 465;
-            $mail->setFrom('dewan201968@gmail.com', 'Sumit'); 
+            $mail->setFrom('GMAIL', 'NAME'); 
             $mail->addAddress($email); 
             $mail->isHTML(true);
             $mail->Subject = "RESET PASSWORD";
